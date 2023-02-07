b43fbf3a2b5bcbf22e71acc78d4cc4f0b88fb336
Lokesh Naidu <113303394+NimmakayalaLokesh@users.noreply.github.com>
2023-02-07 21:21:05 +0530

    STACKS & QUEUES
    8

---
+++ Module-1/You are given a string s consisting of lowercase English letters. A duplicate removal consists of choosing two adjacent and equal letters and removing them. We repeatedly make duplicate removals on s until we no longer can.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char s[20];
+    int i;
+    
+    //Enter the string
+    printf("Enter the string: ");
+    scanf("%s", s);
+    
+    //Loop to check for duplicate characters
+    for(i = 0; i < strlen(s); i++)
+    {
+        if(s[i] == s[i+1])
+        {
+            //Replace the duplicate characters with a null character
+            s[i] = '\0';
+            s[i+1] = '\0';
+            i++;
+        }
+    }
+    
+    //Print the string after removing duplicates
+    printf("String after removing duplicates: %s", s);
+    
+    return 0;
+}
