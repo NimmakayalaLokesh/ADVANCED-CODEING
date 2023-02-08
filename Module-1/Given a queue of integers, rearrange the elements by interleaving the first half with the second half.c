483f10baf9b910ead34218da9fd565e57ecf0c57
Lokesh Naidu <113303394+NimmakayalaLokesh@users.noreply.github.com>
2023-02-08 19:44:07 +0530

    3q

--- Module-1/Given a queue of integers, rearrange the elements by interleaving the first half with the second half.c
+++ Module-1/Given a queue of integers, rearrange the elements by interleaving the first half with the second half.c
@@ -57,3 +57,127 @@ int main() {
     }
     return 0;
 }
+
+
+// 2nd model 
+
+#include<stdio.h>
+int queue[20];
+int stack[20];
+int front=-1,rear=-1,top=-1,fronto=-1,rearo=-1;
+int n;
+int push(int x)
+{	
+	if(top== n-1)
+	printf("\n overflow");
+	else
+	{
+	top++;
+	stack[top]=x;
+	//printf("\n push %d",stack[top]);
+	}
+}
+int pop()
+{	int temp;
+	if(top==-1)
+	{
+	printf("\n underflow");
+	return 9999;
+	}
+	else
+	{
+	 temp= stack[top];  
+	 top--;
+	 return temp;
+	}
+}
+void enqueue(int x)
+{
+   if(front==-1 && rear==-1)
+   {
+      front=rear=0;
+      queue[rear]=x;
+   }
+   else if(rear== n-1)
+   {
+       printf("\nThe queue is full\n");
+   }
+   else
+   {
+       rear=rear+1;
+       queue[rear]=x;
+   }
+}
+int dequeue()
+{
+    int temp;
+    if(front==rear==-1)
+    {
+        printf("\nthe queue is empty\n");
+        return 9999;
+    }
+    
+    else
+    {
+        //printf("\nthe dequed element is %d",queue[front]);
+        temp = queue[front];
+        front=front+1;
+        n=n+1;
+        return temp;
+    }
+}
+int main()
+{
+    int i,n1;
+    scanf("%d",&n);
+    n1=n;
+    for(i=0;i<n1;i++)
+    {
+       int val;
+       scanf("%d",&val);
+       enqueue(val);
+    }
+    int mid=(n1-1)/2;
+    //printf("\n mid %d",mid);
+    for(i=0;i<=mid;i++)
+    {
+        int re=9999;
+        re=dequeue();
+        push(re);
+    }
+    //printf("\nTeja\n");
+    for(i=0;i<=mid;i++)
+    {
+        int re;
+        re=pop();
+        //printf("\n pop re %d",re);
+        enqueue(re);
+    }
+    for(i=0;i<=mid;i++)
+    {
+        int re=9999;
+        re=dequeue();
+        //printf("\n after sq %d",re);
+        enqueue(re);
+    }
+    for(i=0;i<=mid;i++)
+    {
+        int re=dequeue();
+        push(re);
+    }
+    printf("\n");
+    for(i=0;i<n1;i++)
+    {
+        int re;
+        if(i%2==0)
+        {
+            re= pop();
+            printf(" %d ",re);
+        }
+        else{
+            re=dequeue();
+            printf(" %d ",re);
+        }
+    }
+    
+}
